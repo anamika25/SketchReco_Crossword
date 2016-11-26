fae9d5491951d016181e20007984f28d58bb9d45
grewal123 <grewal@tamu.edu>
2016-11-26 16:43:47 -0600

    Adding matrix file

--- src/Utilities/Constants.java
+++ src/Utilities/Constants.java
@@ -3,4 +3,6 @@ package Utilities;
 public class Constants {
 	public static int width = 80;
 	public static int height = 80;
+	public static int com_width = 10;
+	public static int com_height = 10;
 }
