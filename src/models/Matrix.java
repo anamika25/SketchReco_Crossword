5647eca5ea60a55ab78b58c0727c9381301cee05
grewal123 <grewal@tamu.edu>
2016-11-27 17:31:56 -0600

    matrix compresssion

--- src/models/Matrix.java
+++ src/models/Matrix.java
@@ -9,4 +9,39 @@ public class Matrix{
 		orig = new int[Constants.height][Constants.width];
 		comp = new int[Constants.com_height][Constants.com_height];	
 	}
+	
+	void matrixCompression(){
+		int flag = 0;
+		for(int i=0;i<10;i++){
+			for(int j=0;j<10;j++){
+				flag = 0;
+				for(int k=0;k<8;k++){
+					for(int l=0;l<8;l++){
+						if(orig[i*8 + k][j*8 + l]==1)
+							{
+							flag = 1;
+							break;
+							}
+						
+						
+					}
+					if(flag==1)
+						break;
+				}
+				if(flag==1)
+					comp[i][j]=1;
+				else
+					comp[i][j] = 0;
+			}
+			
+		}
+		
+		
+		
+		
+		
+		
+	}
+	
+	
 }
