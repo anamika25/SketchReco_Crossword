1c9151cc332dbecc9691dad2f85a91b5fe43ce21
grewal123 <grewal@tamu.edu>
2016-11-30 15:11:45 -0600

    printing 10x10 Matrix

--- src/controller/CrosswordController.java
+++ src/controller/CrosswordController.java
@@ -91,6 +91,12 @@ public class CrosswordController extends MouseInputAdapter {
     	mat.matrixCompression();
     	int comp[][] = mat.getComp();
     	
+    	for(int i = 0;i<10;i++){
+    		for(int j=0;j<10;j++)
+    			System.out.print(comp[i][j]);
+    		System.out.println();
+    		}
+    	
     	if(!panel.getSketches().contains(currentSketch)) {
     		panel.addSketch(currentSketch);
     	}
